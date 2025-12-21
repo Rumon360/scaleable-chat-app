2b6e28a1b7b9170918989469f8ae7cb2edc65f58
Rumon360 <hmk.rumon@gmail.com>
2025-12-21 20:19:58 +0600

    chore: redirect to dashboard if user goes to auth page after logged in

--- apps/web/src/proxy.ts
+++ apps/web/src/proxy.ts
@@ -3,6 +3,11 @@ import { getSessionCookie } from "better-auth/cookies";
 
 export async function proxy(request: NextRequest) {
   const sessionCookie = getSessionCookie(request);
+  const { pathname } = request.nextUrl;
+
+  if (sessionCookie && (pathname === "/sign-in" || pathname === "/sign-up")) {
+    return NextResponse.redirect(new URL("/dashboard", request.url));
+  }
 
   if (!sessionCookie) {
     return NextResponse.redirect(new URL("/sign-in", request.url));
@@ -12,5 +17,5 @@ export async function proxy(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ["/dashboard"],
+  matcher: ["/dashboard", "/sign-in", "/sign-up"],
 };
