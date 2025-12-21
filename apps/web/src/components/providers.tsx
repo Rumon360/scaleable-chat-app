4f38def114ed4df017df0ee7b41bc7f7aa8bb0dd
Rumon360 <hmk.rumon@gmail.com>
2025-12-21 20:20:19 +0600

    feat: tanstack query

--- apps/web/src/components/providers.tsx
+++ apps/web/src/components/providers.tsx
@@ -1,12 +1,17 @@
 "use client";
 
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { Toaster } from "@/components/ui/sonner";
+
+const queryClient = new QueryClient();
 
 export default function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       {children}
+      <ReactQueryDevtools initialIsOpen={false} />
       <Toaster richColors />
-    </>
+    </QueryClientProvider>
   );
 }
