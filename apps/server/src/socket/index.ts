56fe15111aca37df6a8d2f539dc226dc8cdb54dc
Rumon360 <hmk.rumon@gmail.com>
2025-12-25 18:20:36 +0600

    chore: update socket middleware

--- apps/server/src/socket/index.ts
+++ apps/server/src/socket/index.ts
@@ -1,17 +1,41 @@
+import { auth } from "@scaleable-chat-app/auth";
+import prisma from "@scaleable-chat-app/db";
+import { fromNodeHeaders } from "better-auth/node";
 import type { Server, Socket } from "socket.io";
 
 interface CustomSocket extends Socket {
   room: string;
+  user?: any;
 }
 
 export const setupSocket = (io: Server) => {
-  io.use((socket: Socket, next) => {
-    const room = socket.handshake.auth.room;
-    if (!room) {
-      return next(new Error("Invalid Room!"));
+  io.use(async (socket, next) => {
+    try {
+      const session = await auth.api.getSession({
+        headers: fromNodeHeaders(socket.handshake.headers),
+      });
+
+      if (!session || !session.user) {
+        return next(new Error("Unauthorized: Invalid Session"));
+      }
+
+      const room = socket.handshake.auth.room;
+
+      const chat_room = await prisma.chatGroup.findUnique({
+        where: { id: room },
+      });
+
+      if (!room || !chat_room) {
+        return next(new Error("Invalid Room!"));
+      }
+
+      (socket as CustomSocket).user = session.user;
+      (socket as CustomSocket).room = room;
+
+      next();
+    } catch (error) {
+      next(new Error("Internal Server Error during Auth"));
     }
-    (socket as CustomSocket).room = room;
-    next();
   });
 
   io.on("connection", (socket) => {
