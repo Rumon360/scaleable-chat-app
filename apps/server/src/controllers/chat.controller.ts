0a1af3e83da7c2909f5c7ecf96737cce9d7c5b2f
Rumon360 <hmk.rumon@gmail.com>
2025-12-25 18:06:22 +0600

    update: join room

--- apps/server/src/controllers/chat.controller.ts
+++ apps/server/src/controllers/chat.controller.ts
@@ -3,6 +3,11 @@ import prisma from "@scaleable-chat-app/db";
 import { fromNodeHeaders } from "better-auth/node";
 import { type Request, type Response } from "express";
 
+interface GroupUserType {
+  group_id: string;
+  passcode: string;
+}
+
 export class ChatGroupController {
   // Get All
   static async index(req: Request, res: Response) {
@@ -15,7 +20,7 @@ export class ChatGroupController {
         const user = data.user;
 
         const groups = await prisma.chatGroup.findMany({
-          where: { user_id: user.id },
+          where: { owner_id: user.id },
           orderBy: { createdAt: "desc" },
         });
 
@@ -35,7 +40,10 @@ export class ChatGroupController {
     try {
       const { id } = req.params;
 
-      const group = await prisma.chatGroup.findUnique({ where: { id: id } });
+      const group = await prisma.chatGroup.findUnique({
+        where: { id: id },
+        include: { groupUsers: true },
+      });
 
       return res.json({
         message: "Chat group fetched successfully!",
@@ -61,7 +69,7 @@ export class ChatGroupController {
           data: {
             title: body.title,
             passcode: body.passcode,
-            user_id: user?.id,
+            owner_id: user?.id,
           },
         });
 
@@ -88,7 +96,7 @@ export class ChatGroupController {
         await prisma.chatGroup.update({
           where: {
             id: id,
-            user_id: user.id,
+            owner_id: user.id,
           },
           data: { title: body.title, passcode: body.passcode },
         });
@@ -111,7 +119,7 @@ export class ChatGroupController {
 
       if (data) {
         await prisma.chatGroup.delete({
-          where: { id: id, user_id: data.user.id },
+          where: { id: id, owner_id: data.user.id },
         });
 
         return res.json({
@@ -121,6 +129,62 @@ export class ChatGroupController {
       return res.status(401).json({ error: "Unauthorized!" });
     } catch (error) {
       return res.status(500).json({ message: "Somethign went wrong!" });
+    }
+  }
+
+  // Join user to room
+  static async join_room(req: Request, res: Response) {
+    try {
+      const body: GroupUserType = req.body;
+
+      const data = await auth.api.getSession({
+        headers: fromNodeHeaders(req.headers),
+      });
+
+      if (data) {
+        const userId = data?.user.id;
+
+        const chat_group = await prisma.chatGroup.findUnique({
+          where: { id: body.group_id },
+        });
+
+        if (!chat_group) {
+          return res.status(404).json({ message: "Chat group not found." });
+        }
+
+        if (chat_group.passcode !== body.passcode) {
+          return res.status(401).json({ message: "Invalid passcode." });
+        }
+
+        const existingMembership = await prisma.groupUsers.findUnique({
+          where: {
+            group_id_user_id: {
+              group_id: body.group_id,
+              user_id: userId,
+            },
+          },
+        });
+
+        if (existingMembership) {
+          return res
+            .status(400)
+            .json({ message: "You are already a member of this group." });
+        }
+
+        await prisma.groupUsers.create({
+          data: {
+            group_id: body.group_id,
+            user_id: userId,
+            role: "member",
+          },
+        });
+
+        return res.status(201).json({ message: "Group joined successfully!" });
+      }
+      return res.status(401).json({ error: "Unauthorized!" });
+    } catch (error) {
+      console.error("Join Room Error:", error);
+      return res.status(500).json({ message: "Internal server error." });
     }
   }
 }
