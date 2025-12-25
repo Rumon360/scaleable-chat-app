34e4161fb0d7948ff3b402f8221f7ac1b28ce3d6
Rumon360 <hmk.rumon@gmail.com>
2025-12-25 18:13:00 +0600

    feat: chat ui

--- apps/web/src/components/chat/sidebar-content.tsx
+++ apps/web/src/components/chat/sidebar-content.tsx
@@ -1,29 +1,36 @@
-import React from "react";
 import { ScrollArea } from "@/components/ui/scroll-area";
+import type { GroupChatUserType } from "@/lib/types";
 
-type Props = {};
+type Props = {
+  participants: GroupChatUserType[];
+};
 
-function SidebarContent({}: Props) {
+function SidebarContent({ participants }: Props) {
   return (
     <div className="flex flex-col h-full bg-muted/20">
       <ScrollArea className="flex-1 py-4">
-        <div className="space-y-0.5">
-          <p className="text-sm font-bold uppercase tracking-wider text-muted-foreground px-3 mb-2">
+        <div>
+          <p className="text-sm font-bold tracking-wider text-muted-foreground px-4 mb-2">
             Members
           </p>
-          {/* {chat?.participants?.map((user: any) => (
-            <button
-              key={user.id}
-              className="w-full flex items-center gap-3 px-3 py-2.5 rounded-lg hover:bg-background transition-all group border border-transparent hover:border-border/50 shadow-none hover:shadow-sm"
-            >
-              <div className="h-2 w-2 rounded-full bg-green-500 shrink-0" />
-              <div className="text-left overflow-hidden">
-                <p className="text-sm font-medium truncate group-hover:text-primary transition-colors">
-                  {user.name}
-                </p>
-              </div>
-            </button>
-          ))} */}
+          <div className="px-4 py-2 space-y-2">
+            {participants.map((user) => (
+              <button
+                key={user.name}
+                className="w-full flex items-center gap-3 px-3 py-2.5 bg-background group border border-border shadow-sm"
+              >
+                <div className="text-left overflow-hidden">
+                  <p className="text-sm font-medium truncate group-hover:text-primary transition-colors">
+                    {user.name}
+                  </p>
+                  <p className="text-xs">
+                    Joined:{" "}
+                    <span>{new Date(user.createdAt).toDateString()}</span>
+                  </p>
+                </div>
+              </button>
+            ))}
+          </div>
         </div>
       </ScrollArea>
     </div>
--- apps/web/src/components/chat/sidebar-sheet.tsx
+++ apps/web/src/components/chat/sidebar-sheet.tsx
@@ -3,10 +3,13 @@ import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
 import SidebarContent from "@/components/chat/sidebar-content";
 import { Button } from "@/components/ui/button";
 import { Menu } from "lucide-react";
+import type { GroupChatUserType } from "@/lib/types";
 
-type Props = {};
+type Props = {
+  participants: GroupChatUserType[];
+};
 
-function SidebarSheet({}: Props) {
+function SidebarSheet({ participants }: Props) {
   return (
     <Sheet>
       <SheetTrigger asChild>
@@ -15,7 +18,7 @@ function SidebarSheet({}: Props) {
         </Button>
       </SheetTrigger>
       <SheetContent side="left" className="p-0 w-72">
-        <SidebarContent />
+        <SidebarContent participants={participants} />
       </SheetContent>
     </Sheet>
   );
